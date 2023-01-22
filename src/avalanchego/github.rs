43b132a5df3510feee5635fd8537aea4f1706897
Gyuho Lee <gyuho.lee@avalabs.org>
2023-01-22 22:31:28 +0800

    move 'get_plugin_dir'
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/avalanchego/github.rs
+++ src/avalanchego/github.rs
@@ -236,18 +236,3 @@ pub async fn download(
     }
     Ok(String::from(avalanchego_path.as_os_str().to_str().unwrap()))
 }
-
-///  build
-///    ├── avalanchego (the binary from compiling the app directory)
-///    └── plugins
-///        └── evm
-pub fn get_plugin_dir<P: AsRef<Path>>(avalanche_bin: P) -> String {
-    let parent_dir = avalanche_bin.as_ref().parent().unwrap();
-    String::from(
-        parent_dir
-            .join(Path::new("plugins"))
-            .as_path()
-            .to_str()
-            .unwrap(),
-    )
-}
--- src/avalanchego/mod.rs
+++ src/avalanchego/mod.rs
@@ -1,2 +1,20 @@
 pub mod github;
 pub mod s3;
+
+use std::path::Path;
+
+///  build
+///    ├── avalanchego (the binary from compiling the app directory)
+///    └── plugins
+///        └── evm
+///        └── abc
+pub fn get_plugin_dir<P: AsRef<Path>>(avalanche_bin: P) -> String {
+    let parent_dir = avalanche_bin.as_ref().parent().unwrap();
+    String::from(
+        parent_dir
+            .join(Path::new("plugins"))
+            .as_path()
+            .to_str()
+            .unwrap(),
+    )
+}

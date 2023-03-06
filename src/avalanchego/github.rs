ef389c6ab3af69aeadebc9461ba75044982115c8
Gyuho Lee <gyuho.lee@avalabs.org>
2023-03-06 12:32:47 +0800

    0.0.46
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/avalanchego/github.rs
+++ src/avalanchego/github.rs
@@ -15,7 +15,7 @@ pub async fn download_latest(arch: Option<Arch>, os: Option<Os>) -> io::Result<S
 }
 
 /// ref. <https://github.com/ava-labs/avalanchego/releases>
-pub const DEFAULT_TAG_NAME: &str = "v1.9.10";
+pub const DEFAULT_TAG_NAME: &str = "v1.9.11";
 
 /// Downloads the official "avalanchego" binaries from the GitHub release page.
 /// Returns the path to the binary path.
